854632f6e9c407305887ae3f25cf862d7e8e5e3b
Anastasiako6 <anastasiakorotenko3@gmail.com>
2023-03-07 13:21:03 +0200

    add task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,15 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from "notiflix";
 
 const text = document.querySelector('#datetime-picker');
-const wrapTime = document.querySelector('.timer')
 const buttonStart = document.querySelector('button[data-start]');
 const days = document.querySelector('span[data-days]');
 const hours = document.querySelector('span[data-hours]');
 const minutes = document.querySelector('span[data-minutes]');
 const seconds = document.querySelector('span[data-seconds]')
 
-buttonStart.disable = true;
+buttonStart.disabled = true;
 
 // об'єкт
 const options = {
@@ -18,15 +18,17 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-      if (selectedDates < new Date()) {
-        alert("Please choose a date in the future");
-        buttonStart.disable = true;
+      if (selectedDates[0] < new Date()) {
+         Notiflix.Notify.failure ('Please choose a date in the future');
+        buttonStart.disabled = true;
       } else {
-          buttonStart.disable = false;
+          buttonStart.disabled = false;
+          targetDate = selectedDates[0];
     }
   },
 };
 
+flatpickr(text, options);
 // функція підрахунку значень
 
 function convertMs(ms) {
@@ -48,10 +50,29 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-flatpickr(text, options);
 
+function addNormViewTime(value) {
+    return value.toString().padStart(2, '0');
+}
 
+buttonStart.addEventListener('click', () => {
+    
+    let wrapTime = setInterval(() => {
 
-// buttonStart.addEventListener('click', () => {
-//     let 
-// })
+        let substraction = new Date(text.value) - new Date();
+        buttonStart.disabled = true;
+        console.log(substraction);
+
+        if (substraction > 0) {
+            let timeObject = convertMs(substraction);
+        days.textContent = addNormViewTime(timeObject.days);
+        hours.textContent = addNormViewTime(timeObject.hours);
+        minutes.textContent = addNormViewTime(timeObject.minutes);
+        seconds.textContent = addNormViewTime(timeObject.seconds);
+        } else {
+            Notiflix.Notify.success('Finish');
+            clearInterval(wrapTime);
+    }
+    }, 1000)
+
+})
